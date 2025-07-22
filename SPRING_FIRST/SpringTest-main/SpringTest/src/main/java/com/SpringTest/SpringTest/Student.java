c018b52fbae832fec5f09973a9fd841081c27044
rahi56 <148628500+rahi56@users.noreply.github.com>
2025-07-22 23:14:32 +0600

    Add files via upload

---
+++ SPRING_FIRST/SpringTest-main/SpringTest/src/main/java/com/SpringTest/SpringTest/Student.java
@@ -0,0 +1,20 @@
+package com.SpringTest.SpringTest;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="STUDENT")
+public class Student {
+    @Id
+    @Column(name="ID")
+    private int id;
+    @Column(name="MARK")
+    private int mark;
+    @Column(name="NAME")
+    private String name;
+
+}
